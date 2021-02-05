1f7d233dfb1b5247ba5efd3069c7e8c472b19f31
yushan <yushan@intervest.lk>
2021-02-06 02:10:05 +0530

    feat: Listening to changes

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ function createStore() {
     // 4. Update the state.
 
     let state;
+    let listeners = [];
 
     const getState = () => state;
 
+    const subscribe = (listener) => {
+        listeners.push(listener);
+    }
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
+
+const store = createStore();
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState());
+});
+store.subscribe(() => {
+    console.log('The store changed.');
+})

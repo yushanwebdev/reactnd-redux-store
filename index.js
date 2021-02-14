4851861dabf142888eb344300af56ab7e68f0d53
yushan <yushan@intervest.lk>
2021-02-14 11:46:57 +0530

    refactor: Change if-else to switch

--- index.js
+++ index.js
@@ -32,14 +32,15 @@ function createStore(reducer) {
 
 // App Code
 function todos(state = [], action) {
-    if(action.type === 'ADD_TODO') {
-        return state.concat([action.todo]);
-    } else if(action.type === 'REMOVE_TODO') {
-        return state.filter((todo) => todo.id !== action.id);
-    } else if(action.type === 'TOGGLE_TODO') {
-        return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }))
-    } else {
-        return state;
+    switch(action) {
+        case 'ADD_TODO':
+            return state.concat([action.todo]);
+        case 'REMOVE_TODO':
+            return state.filter((todo) => todo.id !== action.id);
+        case 'TOGGLE_TODO':
+            return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }));
+        case DEFAULT:
+            return state;
     }
 } 
 

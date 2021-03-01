49ccf38718a3eeab0d1a1c3c78396d362b35b15f
yushan <yushan@intervest.lk>
2021-03-01 19:41:30 +0530

    feat: Todos component added

---
+++ src/components/Todos.js
@@ -0,0 +1,46 @@
+import {
+    handleAddTodo,
+    handleDeleteTodo,
+    handleToggleTodo
+} from '../actions/todos';
+import { connect } from 'redux-thunk';
+
+class Todos extends React.Component {
+    addItem = (e) => {
+        e.preventDefault();
+
+        this.props.dispatch(handleAddTodo(
+            this.input.value,
+            () => this.input.value = ''
+        ));
+    }
+
+    removeItem = (todo) => {
+        this.props.dispatch(handleDeleteTodo(todo));
+    }
+
+    toggleItem = (todo) => {
+        this.props.dispatch(handleToggleTodo(todo));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todos</h1>
+                <input
+                    type='text'
+                    placeholder='Add Todo'
+                    ref={(input) => this.input = input} />
+                <button onClick={this.addItem}>Add Todo</button>
+                <List
+                    items={this.props.todos}
+                    remove={this.removeItem}
+                    toggle={this.toggleItem} />
+            </div>
+        )
+    }
+}
+
+export default connect((state) => ({
+    todos: state.todos
+}))(Todos);

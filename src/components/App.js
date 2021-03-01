3e18698f1b16d78be04c15be6c8628139698a76c
yushan <yushan@intervest.lk>
2021-03-01 19:45:55 +0530

    feat: App component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+import { handleInitialData } from '../actions/shared';
+import ConnectedTodos from './Todos';
+import ConnectedGoals from './Goals';
+import { connect } from 'redux-thunk';
+
+class App extends React.Component {
+  componentDidMount() {
+      const { dispatch } = this.props;
+
+      dispatch(handleInitialData());
+  }
+
+  render() {
+      if (this.props.loading === true) {
+          return <h3>Loading...</h3>
+      }
+
+      return (
+          <div>
+              <ConnectedTodos />
+              <ConnectedGoals />
+          </div>
+
+      )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App);

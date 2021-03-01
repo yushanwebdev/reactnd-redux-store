3da63c538607529ff57d3ccb14b3b40de29489b6
yushan <yushan@intervest.lk>
2021-03-01 11:47:36 +0530

    feat: shared actions created

---
+++ src/actions/shared.js
@@ -0,0 +1,20 @@
+const RECEIVE_DATA = 'RECEIVE_DATA';
+
+function receiveDataAction(todos, goals) {
+    return {
+        type: RECEIVE_DATA,
+        todos,
+        goals
+    }
+}
+
+function handleInitialData() {
+    return (dispatch) => {
+        Promise.all([
+            API.fetchTodos(),
+            API.fetchGoals()
+        ]).then(([todos, goals]) => {
+            dispatch(receiveDataAction(todos, goals));
+        });
+    }
+}

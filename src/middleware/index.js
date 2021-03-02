9f9695f2126fd28540fc2b4de819ca57d580da1c
yushan <yushan@intervest.lk>
2021-03-02 12:48:33 +0530

    fix: Fix checker and logger not exported issue

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,5 @@
-import { checker } from './checker';
-import { logger } from './logger';
+import checker from './checker';
+import logger from './logger';
 import { thunk } from 'redux-thunk';
 import { applyMiddleware } from 'redux';
 
